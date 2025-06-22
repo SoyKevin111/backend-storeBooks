a0337924350e8d4ea0e92aa6aec99f310bf28da8
AlvaradoTriviño <oswaldo.alvaradot@ug.edu.ec>
2025-06-22 00:26:46 -0500

    agregando campo name en la clase editoral

--- src/main/java/com/example/strorebooks/catalog/editorial/infraestructure/adapter/out/model/Editorial.java
+++ src/main/java/com/example/strorebooks/catalog/editorial/infraestructure/adapter/out/model/Editorial.java
@@ -1,6 +1,7 @@
 package com.example.strorebooks.catalog.editorial.infraestructure.adapter.out.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -15,5 +16,10 @@ public class Editorial {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    Long id;
+
+   @Column(name = "name", nullable = false)
+   @NotNull(message= "name not null")
+   String name;
+
 
 }
