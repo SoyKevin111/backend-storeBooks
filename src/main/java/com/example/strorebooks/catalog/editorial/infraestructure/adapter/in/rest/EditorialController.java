0be69b597f0b82e5d4dd068c79b27273012da026
AlvaradoTriviño <oswaldo.alvaradot@ug.edu.ec>
2025-06-24 20:51:41 -0500

    Terminado El editorial Service

--- src/main/java/com/example/strorebooks/catalog/editorial/infraestructure/adapter/in/rest/EditorialController.java
+++ src/main/java/com/example/strorebooks/catalog/editorial/infraestructure/adapter/in/rest/EditorialController.java
@@ -1,4 +1,36 @@
 package com.example.strorebooks.catalog.editorial.infraestructure.adapter.in.rest;
 
+
+import com.example.strorebooks.catalog.editorial.application.EditorialMapping;
+import com.example.strorebooks.catalog.editorial.application.EditorialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping()
 public class EditorialController {
+
+    @Autowired
+    private EditorialService editorialService;
+
+    @Autowired
+    private EditorialMapping editorialMapping;
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteEditorial(@PathVariable Long id) {
+        editorialService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllEditorials() {
+        return ResponseEntity.ok(editorialService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findEditorialById(@PathVariable Long id) {
+        return ResponseEntity.ok(editorialService.findById(id));
+    }
 }
+
