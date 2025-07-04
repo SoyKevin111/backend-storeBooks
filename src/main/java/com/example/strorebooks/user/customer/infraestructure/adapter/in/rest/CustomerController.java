46e4a8366596f27e16a0b345c7ca16d5468d2215
nivek679 <kevinherrera976@gmail.com>
2025-07-03 23:03:46 -0500

    RequestMapping cambiado users a customers

--- src/main/java/com/example/strorebooks/user/customer/infraestructure/adapter/in/rest/CustomerController.java
+++ src/main/java/com/example/strorebooks/user/customer/infraestructure/adapter/in/rest/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/storebooks/users")
+@RequestMapping("/storebooks/customers")
 public class CustomerController {
 
    @Autowired
