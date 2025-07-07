a9289203a65a985e707bfce72aa9d58f289ce4d6
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-07-07 11:34:33 -0500

    cambiando validacion del request de author

--- src/main/java/com/example/strorebooks/catalog/booksauthors/infraestructure/adapter/in/dto/AuthorRequest.java
+++ src/main/java/com/example/strorebooks/catalog/booksauthors/infraestructure/adapter/in/dto/AuthorRequest.java
@@ -1,7 +1,9 @@
 package com.example.strorebooks.catalog.booksauthors.infraestructure.adapter.in.dto;
 
 import com.example.strorebooks.catalog.booksauthors.infraestructure.adapter.out.model.State;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -16,16 +18,16 @@ public class AuthorRequest {
     Long id;
 
     @NotBlank(message = "Name must not be null or empty")
-    @Size(max = 15, message = "Name must not exceed 15 characters")
+    @Size(max = 30, message = "Name must not exceed 15 characters")
     String name;
     @NotBlank(message = "Lastname must not be null or empty")
-    @Size(max = 15, message = "Lastname must not exceed 15 characters")
+    @Size(max = 30, message = "Lastname must not exceed 15 characters")
     String lastname;
     @NotBlank(message = "Email must not be null or empty")
-    @Size(max = 50, message = "Email must not exceed 50 characters")
+    @Email(message = "Email must be a valid email address")
     String email;
-    @NotBlank(message = "Phone must not be null or empty")
-    @Size(max = 10, message = "Phone must not exceed 10 characters")
+    @NotBlank(message = "Identification number must not be null or empty")
+    @Pattern(regexp = "\\d{10}", message = "IdentificationNUmber must be exactly 10 digits")
     String numberIdentification;
 
     State state;
